56b45625be2fb8139ec7aa50fdfb762caa02064c
Karuppasamy Palanikumar <ios@trioangle.com>
2022-03-01 14:54:05 +0530

    Test Commit

---
+++ GuesturesTrioangle/GuesturesTrioangle.h
@@ -0,0 +1,18 @@
+//
+//  GuesturesTrioangle.h
+//  GuesturesTrioangle
+//
+//  Created by Trioangle on 22/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GuesturesTrioangle.
+FOUNDATION_EXPORT double GuesturesTrioangleVersionNumber;
+
+//! Project version string for GuesturesTrioangle.
+FOUNDATION_EXPORT const unsigned char GuesturesTrioangleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GuesturesTrioangle/PublicHeader.h>
+
+
---
+++ GuesturesTrioangle/Logger.swift
@@ -0,0 +1,22 @@
+//
+//  Logger.swift
+//  GuesturesTrioangle
+//
+//  Created by Trioangle on 22/02/22.
+//
+
+import Foundation
+
+public
+class Logger : NSObject {
+    
+    public
+    func printerfuntion(content: String) {
+        print(content)
+    }
+}
+
+public
+func printerfuntion(content: String) {
+    print(content)
+}
